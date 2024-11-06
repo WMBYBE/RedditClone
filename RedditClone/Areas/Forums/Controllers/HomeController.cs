5d69daa4ab518b8fab2a594e7d30fa0a65f50dc8
WMBYBE <williambbrouillette@gmail.com>
2024-11-06 13:23:03 -0600

    created area home controllers

---
+++ RedditClone/Areas/Forums/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedditClone.Models;
+using System.Diagnostics;
+
+namespace RedditClone.Areas.Forums.Controllers
+{
+    [Area("Forum")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RedditClone/Areas/Posts/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedditClone.Models;
+using System.Diagnostics;
+
+namespace RedditClone.Areas.Posts.Controllers
+{
+    [Area("Posts")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
