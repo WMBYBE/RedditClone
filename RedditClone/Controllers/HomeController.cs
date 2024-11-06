705422452c35d58614630c6985bf61ae97b95dab
WMBYBE <williambbrouillette@gmail.com>
2024-11-06 14:43:11 -0600

    more index

--- RedditClone/Controllers/HomeController.cs
+++ RedditClone/Controllers/HomeController.cs
@@ -6,11 +6,21 @@ namespace RedditClone.Controllers
 {
     public class HomeController : Controller
     {
+        private ForumDbContext context { get; set; }
+
+        public HomeController(ForumDbContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var forums= context.Forums
+                .ToList();
+            return View(forums);
         }
-        [Route("Forums/[controller]/[action]/[id]")]
+        
+        [Route("forums/[controller]/[action]/[id]")]
         public IActionResult forums(string id)
         {
             return Content("ForumController, List action, Category: " + id);
