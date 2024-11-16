59594858afc33ebefcb7d438c6fb0401e74bc3a7
WMBYBE <williambbrouillette@gmail.com>
2024-11-15 23:30:07 -0600

    updated users

--- RedditClone/Models/User.cs
+++ RedditClone/Models/User.cs
@@ -9,4 +9,7 @@ public class User
 
     [Required(ErrorMessage = "Please enter a UserName.")]
     public string? Name { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+
 }
