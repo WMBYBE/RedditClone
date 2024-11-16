1c5c7fe636f77bf3653e3d61bcb745d8c6c556cd
Switchblade4703 <156360989+Switchblade4703@users.noreply.github.com>
2024-11-16 15:32:34 -0600

    Syncronizing

--- RedditClone/Areas/Forums/Controllers/ForumController.cs
+++ RedditClone/Areas/Forums/Controllers/ForumController.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RedditClone.Models;
 
 namespace RedditClone.Areas.Forums.Controllers
 {
     [Area("Forums")]
     public class ForumController : Controller
     {
-        
+        private ForumDbContext context { get; set; }
+
+        public ForumController(ForumDbContext ctx)
+        {
+            context = ctx;
+        }
+
         [Route("[action]/{id?}")]
         public ActionResult index(int id)
         {
             //PAGE 219                                                  
-            return View("Index");
+            var forum = context.Posts.OrderBy(c => c.PostId).ToList();
+            return View(forum);
+
         }
 
     }
