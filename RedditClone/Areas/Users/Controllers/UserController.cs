ee4d7a38218fc30c1f80d54956e157621bdd693c
WMBYBE <williambbrouillette@gmail.com>
2024-11-18 13:49:14 -0600

    added page for viewing a user's posts

--- RedditClone/Areas/Users/Controllers/UserController.cs
+++ RedditClone/Areas/Users/Controllers/UserController.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedditClone.Models;
 
 namespace RedditClone.Areas.Users.Controllers
 {
     public class UserController : Controller
-    { 
+    {
+        private ForumDbContext context { get; set; }
+
+        public UserController(ForumDbContext ctx)
+        {
+            context = ctx;
+        }
+        public ActionResult Index(int id)
+        {
+            var posts = context.Posts
+                    .Where(p => p.User.UserId == id)
+                    .OrderBy(p => p.PostId);
+
+            ViewBag.User = context.Users.Find(id);
+            return View(posts.ToList());
+        }
 
     }
 }
