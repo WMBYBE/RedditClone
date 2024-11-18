f252bd85d388170be714041ad352e80fcd1d214b
Switchblade4703 <156360989+Switchblade4703@users.noreply.github.com>
2024-11-17 18:22:32 -0600

    Bootstrap added

--- RedditClone/Migrations/20241118001333_initial.Designer.cs
+++ RedditClone/Migrations/20241118001333_initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RedditClone.Models;
 
@@ -11,9 +12,10 @@ using RedditClone.Models;
 namespace RedditClone.Migrations
 {
     [DbContext(typeof(ForumDbContext))]
-    partial class ForumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241118001333_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
